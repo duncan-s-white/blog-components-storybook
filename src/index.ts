e0f394dd9c5ea9d5be07b1696c63eccfd81e7ec5
duncan-s-white <duncanstanleywhite@gmail.com>
2024-12-24 10:34:44 +0000

    Build: (4e19e89) update permissions in yml

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as PageTitle } from "./components/PageTitle";
-export { default as Paragraph } from "./components/Paragraph";
-export { default as Link } from "./components/Link";
-export { default as Image } from "./components/Image";
-export { default as Card } from "./components/Card";
