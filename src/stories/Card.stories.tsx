06a8761a2093883a0d989dcd0e95777dbc489813
Duncan White <duncanstanleywhite@gmail.com>
2024-12-28 12:01:16 +0000

    update card story

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -14,9 +14,11 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   children: { control: 'text' },
-  // }
+  argTypes: {
+    imgSrc: { 
+      options: ['/static/images/react_next.png', '/static/images/about_2.png'],
+      control: { type: 'radio'} },
+  }
   // // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   // args: { onClick: fn() },
 } satisfies Meta<typeof Card>;
